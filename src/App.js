c504aecb99401967e8a08f5be4db8184822e9943
ImranHossain5722 <imd572258@gmail.com>
2022-05-30 18:51:00 +0600

    change git repo

--- src/App.js
+++ src/App.js
@@ -17,12 +17,12 @@ function App() {
     <div className="App">
       <Header></Header>
       <Routes>
-          <Route path='/' element={<Home></Home>}></Route>
-          <Route path='/home' element={<Home></Home>} ></Route>
-          <Route path='about' element={<AboutMe></AboutMe>}></Route>
-          <Route path='blogs' element={<Blogs></Blogs>} ></Route>
-          <Route path='login' element={<Login></Login>}></Route>
-          <Route path='register'element={<Register></Register>}></Route>
+          <Route path='/' element= {<Home></Home>}></Route>
+          <Route path='/home' element= {<Home></Home>} ></Route>
+          <Route path='about' element= {<AboutMe></AboutMe>}></Route>
+          <Route path='blogs' element= { <Blogs></Blogs>} ></Route>
+          <Route path='login' element= { <Login></Login>}></Route>
+          <Route path='register' element= {<Register></Register>}></Route>
           
           <Route path='checkout'element={<RequireAuth>
             <CheckOut></CheckOut>
